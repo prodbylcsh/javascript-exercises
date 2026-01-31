60e57c2444eebb47d8e1bbe0269ef5e57dea1450
Lukáš Schöbel <seblis@seznam.cz>
2026-01-31 11:22:31 +0100

    Foundations exercise 06 passed

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(repeat, num) {
+    if (num < 0 || typeof num !== "number" || typeof repeat !== "string") {
+        return "ERROR"
+    }
 
+    let string = ""
+
+    for (let i = 0; i < num; i++) {
+        string += repeat
+    }
+
+    return string
 };
 
 // Do not edit below this line
