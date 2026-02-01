87c15ff04a146d1f78d36be4d290875d116f0eba
Lukáš Schöbel <seblis@seznam.cz>
2026-02-01 15:36:26 +0100

    Foundations exercise 12 passed

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0
+
+  for (i = 0; i < array.length; i++) {
+    sum += array[i]
+  }
+
+  return sum
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let result = array[0]
 
+  for (i = 1; i < array.length; i++) {
+    result *= array[i]
+  }
+
+  return result
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return Math.pow(base, power)
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) {
+    return 1
+  }
+
+  let result = number
+
+  for (let i = number; i > 1; i--) {
+    result = result * (i - 1)
+  }
+
+  return result
 };
 
 // Do not edit below this line
