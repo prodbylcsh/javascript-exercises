c3fcb29569f050dae4309066d06194b3ccaa0a0d
Lukáš Schöbel <seblis@seznam.cz>
2026-02-01 19:34:50 +0100

    Foundations exercise 16 passed

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let highestAge = -1
+    let oldestPerson = {}
 
+    array.forEach(element => {
+        let birth = element.yearOfBirth
+        let death = Object.hasOwn(element, "yearOfDeath") ? element.yearOfDeath : new Date().getFullYear()
+
+        let age = death - birth
+
+        if (age > highestAge) {
+            highestAge = age
+            oldestPerson = element
+        }
+    });
+
+    return oldestPerson
 };
 
 // Do not edit below this line
