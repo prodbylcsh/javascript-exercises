089a4c138b884d27ed112f1b3b0e7398f6cb298a
Lukáš Schöbel <seblis@seznam.cz>
2026-02-01 15:11:37 +0100

    Foundations exercise 11 passed

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    const unit = 32
+    const celsius = (temp -unit) * (5/9)
+
+    return +celsius.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const unit = 32
+  const fahrenheit = (temp * (9/5) + 32)
+
+  return +fahrenheit.toFixed(1)
 };
 
 // Do not edit below this line
