f1d6f9118e0b1f5e4299c7e8a619cd21d2a53f92
Lukáš Schöbel <seblis@seznam.cz>
2026-02-01 19:03:21 +0100

    Foundations exercise 14 passed

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = +number
+    if (number === 0) {
+        return 0
+    } else if (number < 0) {
+        return "OOPS"
+    }
 
+    let counter = 2
+    let current = 1
+    let previous = 1
+    let subprevious = 1
+    let result = 1
+    console.log(`Hledám ${number}. člena posloupnosti`)
+
+    while (counter < number) {
+        console.log(`Jedu ${counter}. člena posloupnosti`)
+        subprevious = previous
+        previous = result
+
+        console.log(`Počítám ${subprevious} + ${previous}`)
+        result = subprevious + previous
+
+        counter++ 
+    }
+
+    return result
 };
 
 // Do not edit below this line
