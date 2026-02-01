b5459cdc60a57b4eac3ea72bd57b3ced9f72134f
Lukáš Schöbel <seblis@seznam.cz>
2026-02-01 18:26:19 +0100

    Foundations exercise 13 passed

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleaned = string.replaceAll(/[^a-zA-Z1-9]/gm, "")
+    const stringLength = cleaned.length
 
+    let firstPart = "", secondPart = ""
+    
+    if (stringLength >= 0) {
+        cleaned = cleaned.toLowerCase()
+        firstPart = cleaned.substring(0, Math.floor(stringLength/2))
+        secondPart = cleaned.substring(Math.ceil(stringLength/2), stringLength)
+        const reversed = Array.from(secondPart).reverse().join("")
+
+        if (firstPart === reversed) {
+            return true
+        } else {
+            return false
+        }
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
