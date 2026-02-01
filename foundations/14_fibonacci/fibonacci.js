670d892aeb10acd02e4f67deccc33fd0c29cfda5
Lukáš Schöbel <seblis@seznam.cz>
2026-02-01 19:04:08 +0100

    Cleaned code in exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -7,18 +7,13 @@ const fibonacci = function(number) {
     }
 
     let counter = 2
-    let current = 1
     let previous = 1
     let subprevious = 1
     let result = 1
-    console.log(`Hledám ${number}. člena posloupnosti`)
 
     while (counter < number) {
-        console.log(`Jedu ${counter}. člena posloupnosti`)
         subprevious = previous
         previous = result
-
-        console.log(`Počítám ${subprevious} + ${previous}`)
         result = subprevious + previous
 
         counter++ 
